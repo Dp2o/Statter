5b83119a916da57813a90c59eb0abcbbaa706acf
Dp2o <snufylupicus@gmail.com>
2025-05-12 11:26:50 +1200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -53,6 +53,7 @@ let player = {
 };
 
 // --- Start Button ---
+document.getElementById("PlayButton").style.display = "none";
 document.getElementById("PlayButton").addEventListener("click", startGame);
 
 function startGame() {
