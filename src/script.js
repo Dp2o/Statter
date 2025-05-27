01ac76f6e68c3dcdecee99c0fe5f0addf58b6fde
Dp2o <snufylupicus@gmail.com>
2025-05-27 14:14:33 +1200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ let Level = 1;
 let Experience = 0;
 let NeededExperience = 100;
 let Coins = 100;
-let WalkSpeed = 2;
+let WalkSpeed = 10;
 
 // --- 2. Secondary Stats ---
 let Luck = 1; // Luck now affects upgrade rarity
