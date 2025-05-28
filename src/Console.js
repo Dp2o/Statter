27e585ca141f96ab7838d75b05872abdced5f8a2
Dp2o <snufylupicus@gmail.com>
2025-05-29 09:39:10 +1200

    Update Console.js

--- src/Console.js
+++ src/Console.js
@@ -1,1 +1,21 @@
+function toggleConsole() {
+    const cc = document.getElementById('customConsole');
+    cc.style.display = cc.style.display === 'none' ? 'block' : 'none';
+}
 
+// Custom log function
+function myConsoleLog(...args) {
+    const cc = document.getElementById('customConsole');
+    cc.style.display = 'block';
+    cc.innerHTML += args.map(a => 
+        typeof a === 'object' ? JSON.stringify(a) : a
+    ).join(' ') + '<br>';
+    cc.scrollTop = cc.scrollHeight;
+}
+
+// Optionally override window.console.log (be careful!)
+window.console.log = myConsoleLog;
+
+// Usage
+console.log("Hello, this is your custom console!");
+console.log({foo: "bar"}, [1,2,3]);
